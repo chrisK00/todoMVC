f1901521b77ba4f2a63fb4bee0a53398739d62d0
chris <christianhunter707@gmail.com>
2021-02-20 20:34:09 +0100

    Init

---
+++ todoMVC.DL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using todoMVC.DL.Models;
+
+namespace todoMVC.DL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ todoMVC.PL/Controllers/TodosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using todoMVC.DL;
+
+namespace todoMVC.PL.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TodosController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Todos.ToListAsync());
+        }
+    }
+}
