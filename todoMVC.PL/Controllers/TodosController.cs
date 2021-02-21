1b83ff2892e3869b8328d57d005357f90b9a315e
chris <christianhunter707@gmail.com>
2021-02-21 20:38:28 +0100

    add todo

--- todoMVC.PL/Controllers/TodosController.cs
+++ todoMVC.PL/Controllers/TodosController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using todoMVC.DL;
+using todoMVC.DL.Models;
 
 namespace todoMVC.PL.Controllers
 {
@@ -18,7 +19,18 @@ namespace todoMVC.PL.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            ViewBag.Title = "";
             return View(await _context.Todos.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddTodo(string title)
+        {
+
+            var todoToAdd = new Todo() { Title = title };
+            await _context.AddAsync(todoToAdd);
+            await _context.SaveChangesAsync();
+            return Redirect(nameof(Index));
         }
     }
 }
